75242e7249400d84349e490fd4894dad3114f45f
Бондарчук Денис <bondarchukwd@gmail.com>
2023-05-18 16:51:01 +0300

    Добавлена разворот FRONT приложения в docker

--- front/src/App.js
+++ front/src/App.js
@@ -1,25 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [message, setMessage] = useState('');
+
+    useEffect(() => {
+        fetch('http://localhost:8081/hello')
+            .then(response => response.json())
+            .then(data => setMessage(data.message))
+    })
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>{message}</p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
